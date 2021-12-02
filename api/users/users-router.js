3cb895688595927e1f999d6c151830128480401c
beefybroccoli <tedkimemail@yahoo.com>
2021-12-01 23:25:03 -0800

    ...

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,6 +3,7 @@ const middleware = require("../middleware/middleware");
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 const modelUser = require("./users-model");
+const modelPost = require("../posts/posts-model");
 
 const router = express.Router();
 
@@ -26,20 +27,38 @@ router.post("/", middleware.validateUser, async (req, res) => {
   res.status(201).json(newUser);
 });
 
-router.put("/:id", (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-});
+router.put(
+  "/:id",
+  middleware.validateUserId,
+  middleware.validateUser,
+  async (req, res) => {
+    // RETURN THE FRESHLY UPDATED USER OBJECT
+    // this needs a middleware to verify user id
+    // and another middleware to check that the request body is valid
+    const { id } = req.params;
+    const { name } = req.body;
+    const modifiedUser = await modelUser.update(id, { name });
+    res.status(201).json(modifiedUser);
+  }
+);
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", middleware.validateUserId, async (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  const { id } = req.params;
+  const result = await modelUser.remove(id);
+  res.status(200).json(result);
 });
 
-router.get("/:id/posts", (req, res) => {
+router.get("/:id/posts", middleware.validateUserId, async (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  const { id } = req.params;
+  const posts = await modelPost.get();
+  console.log("id = ", id, ", posts.length = ", posts.length);
+  res
+    .status(200)
+    .json(posts.filter((element) => String(element.user_id) === String(id)));
 });
 
 router.post("/:id/posts", (req, res) => {
