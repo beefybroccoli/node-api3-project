584ba7c3952cd33dfe26e56603c65e991afae8de
beefybroccoli <tedkimemail@yahoo.com>
2021-12-01 22:45:42 -0800

    ...

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,31 @@
+const modelUsers = require("../users/users-model");
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log("logger middleware");
+  console.log(`${req.method} ${req.path} at ${Date.now()}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  console.log("validateUserId middleware");
+  const { id } = req.params;
+  if (id) {
+    const user = await modelUsers.getById(id);
+    req.user = user;
+    next();
+  } else {
+    res.status(404).json({ message: `${id} not found` });
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  console.log("validateUser middleware");
+  next();
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  console.log("validatePost middleware");
+  next();
 }
 
-// do not forget to expose these functions to other modules
+module.exports = { logger, validatePost, validateUser, validateUserId };
--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
+server.use(express.json());
+const routeUsers = require("./users/users-router");
 
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.use("/api/users", routeUsers);
+
+// do not forget to export the router
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
